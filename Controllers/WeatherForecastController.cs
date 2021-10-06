d81b67370c20c31cee031c3c2eb4ca55414eb916
isfenixrojo <isfenixrojo@mail.com>
2021-10-06 02:28:16 -0500

    se crea CRUD completo de API usando la CLI

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -11,14 +11,27 @@ namespace ContosoPizza.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        private static readonly string[]
+            Summaries =
+                new []
+                {
+                    "Freezing",
+                    "Bracing",
+                    "Chilly",
+                    "Cool",
+                    "Mild",
+                    "Warm",
+                    "Balmy",
+                    "Hot",
+                    "Sweltering",
+                    "Scorching"
+                };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(
+            ILogger<WeatherForecastController> logger
+        )
         {
             _logger = logger;
         }
@@ -27,13 +40,15 @@ namespace ContosoPizza.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Enumerable
+                .Range(1, 5)
+                .Select(index =>
+                    new WeatherForecast {
+                        Date = DateTime.Now.AddDays(index),
+                        TemperatureC = rng.Next(-20, 55),
+                        Summary = Summaries[rng.Next(Summaries.Length)]
+                    })
+                .ToArray();
         }
     }
 }
